7cb76e3112be9814f37f21ce8a29f6fd94c87b62
Norbert515 <kozsir.norbert@gmail.com>
2025-04-14 18:08:04 +0200

    Remove vide dependency

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import desktop_drop
-import pasteboard
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
-  PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  desktop_drop
-  pasteboard
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
